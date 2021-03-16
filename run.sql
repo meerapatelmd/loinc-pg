b2080e353840fce205b3dee5618dde00e8480d56
Meera Patel <mpatel@meeras-mbp.home>
2021-03-16 17:07:55 -0400

    update SQL from bundled download

--- run.sql
+++ run.sql
@@ -1,77 +1,69 @@
-DROP SCHEMA loinc CASCADE;
-CREATE SCHEMA loinc;
-SET search_path to loinc;
-
-DROP  TABLE IF EXISTS source_organization;
-CREATE TABLE source_organization (
-id integer primary key,
+SET search_path TO LOINC;
+DROP TABLE IF EXISTS SourceOrganization;
+CREATE TABLE SourceOrganization (
+  id  integer,
   copyright_id varchar(255) not null,
-  name varchar(255) default null,
+  name varchar(255),
   copyright text,
   terms_of_use text,
-  url varchar(255) default null
-) ;
+  url text,
+  primary key (copyright_id)
+);
 
-
-DROP  TABLE IF EXISTS loinc;
-CREATE TABLE loinc (
-  loinc_num varchar(10) PRIMARY KEY,
-  component varchar(255) default null,
-  property varchar(30) default null,
-  time_aspct varchar(15) default null,
-  system varchar(255) default null,
-  scale_typ varchar(30) default null,
-  method_typ varchar(50) default null,
-  class varchar(20) default null,
-  VersionLastChanged varchar(10) default null,
-  chng_type varchar(3) default null,
+DROP TABLE IF EXISTS Loinc;
+CREATE TABLE Loinc (
+  loinc_num varchar(10) not null,
+  component varchar(255),
+  property varchar(255),
+  time_aspct varchar(255),
+  system varchar(255),
+  scale_typ varchar(255),
+  method_typ varchar(255),
+  class varchar(255),
+  VersionLastChanged varchar(255),
+  chng_type varchar(255),
   DefinitionDescription text,
-  status varchar(11) default null,
-  consumer_name varchar(255) default null,
-  "classtype" integer default null,
+  status varchar(255),
+  consumer_name varchar(255),
+  classtype integer,
   formula text,
-  species varchar(20) default null,
   exmpl_answers text,
   survey_quest_text text,
-  survey_quest_src varchar(50) default null,
-  unitsrequired varchar(1) default null,
-  submitted_units varchar(30) default null,
+  survey_quest_src varchar(50),
+  unitsrequired varchar(1),
+  submitted_units varchar(30),
   relatednames2 text,
-  shortname varchar(255) default null,
-  order_obs varchar(15) default null,
-  cdisc_common_tests varchar(1) default null,
-  hl7_field_subfield_id varchar(50) default null,
+  shortname varchar(255),
+  order_obs varchar(15),
+  cdisc_common_tests varchar(1),
+  hl7_field_subfield_id varchar(50),
   external_copyright_notice text,
-  example_units varchar(255) default null,
-  long_common_name varchar(255) default null,
+  example_units varchar(255),
+  long_common_name varchar(255),
   UnitsAndRange text,
-  document_section varchar(255) default null,
-  example_ucum_units varchar(255) default null,
-  example_si_ucum_units varchar(255) default null,
-  status_reason varchar(9) default null,
+  example_ucum_units varchar(255),
+  example_si_ucum_units varchar(255),
+  status_reason varchar(9),
   status_text text,
   change_reason_public text,
-  common_test_rank integer default null,
-  common_order_rank integer default null,
-  common_si_test_rank integer default null,
-  hl7_attachment_structure varchar(15) default null,
-  ExternalCopyrightLink varchar(255) default null,
-  PanelType varchar(50) default null,
-  AskAtOrderEntry varchar(255) default null,
-  AssociatedObservations varchar(255) default null,
-  VersionFirstReleased varchar(10) default null,
-  ValidHL7AttachmentRequest varchar(50) default null
-) ;
+  common_test_rank integer,
+  common_order_rank integer,
+  common_si_test_rank integer,
+  hl7_attachment_structure varchar(15),
+  ExternalCopyrightLink varchar(255),
+  PanelType varchar(50),
+  AskAtOrderEntry varchar(255),
+  AssociatedObservations varchar(255),
+  VersionFirstReleased varchar(255),
+  ValidHL7AttachmentRequest varchar(50),
+  DisplayName varchar(255),
+  
+  primary key (loinc_num)
 
-COMMENT ON COLUMN loinc.loinc_num IS 'The unique LOINC Code is a string in the format of nnnnnnnn-n';
-COMMENT ON COLUMN loinc.component IS 'First major axis-component or analyte';
-COMMENT ON COLUMN loinc.property IS 'Second major axis-property observed (e.g., mass vs. substance)';
-COMMENT ON COLUMN loinc.time_aspct IS 'Third major axis-timing of the measurement (e.g., point in time vs 24 hours)';
-COMMENT ON COLUMN loinc.system IS 'Fourth major axis-type of specimen or system (e.g., serum vs urine)';
-COMMENT ON COLUMN loinc.scale_typ IS 'Fifth major axis-scale of measurement (e.g., qualitative vs. quantitative)';
-COMMENT ON COLUMN loinc.method_typ IS 'Sixth major axis-method of measurement';
-DROP TABLE IF EXISTS map_to;
-CREATE TABLE map_to (
+);
+
+DROP TABLE IF EXISTS MapTo;
+CREATE TABLE MapTo (
   loinc varchar(10) NOT NULL,
   map_to varchar(10) NOT NULL,
   comment text,
@@ -79,10 +71,13 @@ CREATE TABLE map_to (
 
 );
 
+COPY Loinc (loinc_num, component, property, time_aspct, system, scale_typ, method_typ, class, VersionLastChanged, chng_type, DefinitionDescription, status, consumer_name, classtype, formula, exmpl_answers, survey_quest_text, survey_quest_src, unitsrequired, submitted_units, relatednames2, shortname, order_obs, cdisc_common_tests, hl7_field_subfield_id, external_copyright_notice, example_units, long_common_name, UnitsAndRange, example_ucum_units, example_si_ucum_units, status_reason, status_text, change_reason_public, common_test_rank, common_order_rank, common_si_test_rank, hl7_attachment_structure, ExternalCopyrightLink, PanelType, AskAtOrderEntry, AssociatedObservations, VersionFirstReleased, ValidHL7AttachmentRequest, DisplayName ) FROM '/Users/mpatel/Desktop/Loinc_2.69_Text_2.69/Loinc.csv' CSV HEADER;
 
-COMMENT ON COLUMN map_to.loinc IS 'The deprecated term to which the replacement term(s) apply.';
-COMMENT ON COLUMN map_to.map_to IS 'A replacement term that is to be used in place of the deprecated or discouraged';
+COPY MapTo (loinc, map_to, comment) FROM '/Users/mpatel/Desktop/Loinc_2.69_Text_2.69/MapTo.csv' CSV HEADER;
 
-\copy loinc FROM 'loinc.csv' CSV HEADER quote '"';
-\copy map_to FROM 'map_to.csv' CSV HEADER quote '"';
-\copy source_organization FROM 'source_organization.csv' CSV HEADER quote '"';
+
+COPY SourceOrganization (id, copyright_id, name, copyright, terms_of_use, url) FROM '/Users/mpatel/Desktop/Loinc_2.69_Text_2.69/SourceOrganization.csv' CSV HEADER;
+
+select count(*) from Loinc;
+select count(*) from MapTo;
+select count(*) from SourceOrganization;
